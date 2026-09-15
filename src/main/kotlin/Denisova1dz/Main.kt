3e61a0f2b557b37677b1cd65221662f5cf6eef87
Elisaveta Denisova <elisavvv@users.noreply.github.com>
2026-09-15 19:04:47 +0500

    Написала функции 4, 5, 6, 7 и 8

--- src/main/kotlin/Denisova1dz/Main.kt
+++ src/main/kotlin/Denisova1dz/Main.kt
@@ -1,10 +1,24 @@
 package Denisova1dz
 
 import Denisova1dz.parser.CsvParser
+import java.io.File
 
 fun main() {
     val result = CsvParser.parse("src/main/resources/fakePlayersDirty.csv")
 
     println("Успешно прочли игроков: ${result.players.size}")
     println("Пропустили невалидных строк: ${result.skippedCount}")
+
+    val file = File("src/main/resources/fakePlayers.csv")
+    val parseResult = CsvParser.parse("src/main/resources/fakePlayers.csv")
+    val resolver = Denisova1dz.resolver.Resolver(parseResult.players)
+
+    println("1. Без агентства: ${resolver.getCountWithoutAgency()}")
+    println("2. Лучший защитник-бомбардир: ${resolver.getBestScorerDefender()}")
+    println("3. Позиция самого дорогого немца: ${resolver.getTheExpensiveGermanPlayerPosition()}")
+    println("4. Больше всего красных карточек у команды: ${resolver.getTheRudestTeam()}")
+    println("5. Средняя стоимость по позициям: ${resolver.getAverageTransferCostByPosition()}")
+    println("6. Топ-3 полезных игрока: ${resolver.getMostValuablePlayers().map { it.name }}")
+    println("7. Популярные агентства по странам: ${resolver.getMostPopularAgencyByCountry()}")
+    println("8. Доли голов по медиане стоимости: ${resolver.getGoalsShareByMedianCost()}")
 }
--- src/main/kotlin/Denisova1dz/resolver/Resolver.kt
+++ src/main/kotlin/Denisova1dz/resolver/Resolver.kt
@@ -23,11 +23,65 @@ class Resolver  (private val players: List<Player>) : IResolver {
         ?.position
         ?.title ?: "Неизвестно"
 
+    // команда с наибольшим средним числом красных карточек на одного игрока
+    override fun getTheRudestTeam(): Team = players
+        .groupBy { it.team } // группируем игроков по командам
+        .maxByOrNull { it.value.map { player -> player.redCards }.average() }!!
+        .key
 
-    // Заглушки для остальных функций:
-    override fun getTheRudestTeam(): Team = TODO()
-    override fun getAverageTransferCostByPosition(): Map<Position, Double> = TODO()
-    override fun getMostValuablePlayers(): List<Player> = TODO()
-    override fun getMostPopularAgencyByCountry(): Map<String, String> = TODO()
-    override fun getGoalsShareByMedianCost(): Pair<Double, Double> = TODO()
+    // средняя трансферная стоимость по позициям, отсортировывая позиции по убыванию средней стоимости
+    override fun getAverageTransferCostByPosition(): Map<Position, Double> = players
+        .groupBy { it.position }
+        .mapValues { (_, positionPlayers) ->
+            positionPlayers.map { it.transfer }.average()
+        }
+        .entries
+        .sortedByDescending { it.value }
+        .associate { it.key to it.value }
+
+    // топ-3 самых полезных игроков по формуле «голы + 2 * голевые передачи»
+    //    При равенстве полезности раньше в списке стоит игрок, чьё имя идёт раньше по алфавиту
+    override fun getMostValuablePlayers(): List<Player> = players
+        .sortedWith(compareBy(
+            { -(it.goals + 2 * it.assists) },
+            {it.name}
+        ))
+        .take(3)
+
+    // для каждой страны вернуть самое популярное среди её игроков агентство,
+    // при равенстве выбераем агентство, чьё название идёт раньше по алфавиту
+    override fun getMostPopularAgencyByCountry(): Map<String, String> = players
+        .filter { it.agency != null } // есть агентство
+        .groupBy { it.country }      // группируем игроков по их странам
+        .mapValues {
+            it.value                 // it.value — список всех игроков текущей страны
+                .groupBy { player -> player.agency!! } // группируем игроков этой страны по их агентствам
+                .entries             // Агентство = Список его игроков
+                .sortedWith(compareBy(
+                    { agencyEntry -> -agencyEntry.value.size }, // минус сортирует по убыванию числа клиентов (чем больше, тем выше)
+                    { agencyEntry -> agencyEntry.key }          // сортируем по алфавиту названия агентства
+                ))
+                .first().key         // 8. Берем первое (самое популярное) агентство из отсортированного списка
+        }
+
+    // разделите игроков медианой трансферной стоимости и верните пару долей:
+    //    доля голов, забитых игроками не дороже медианы, и доля голов остальных.
+    override fun getGoalsShareByMedianCost(): Pair<Double, Double> {
+    // сортировка стоимости всех игроков для поиска медианы
+    val sortedTransfers = players.map { it.transfer }.sorted()
+    val median = sortedTransfers[sortedTransfers.size / 2]
+    // общее количество голов всех игроков
+    val totalGoals = players.sumOf { it.goals }.toDouble()
+    if (totalGoals == 0.0) return Pair(0.0, 0.0) // от деления на ноль
+    // голы игроков не дороже медианы
+    val goalsCheap = players
+        .filter { it.transfer <= median }
+        .sumOf { it.goals }
+    // голы игроков дороже медианы
+    val goalsExpensive = players
+        .filter { it.transfer > median }
+        .sumOf { it.goals }
+    // доля дешевых , доля дорогих
+    return Pair(goalsCheap / totalGoals, goalsExpensive / totalGoals)
+}
 }
